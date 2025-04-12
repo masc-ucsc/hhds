27b5fc7878f749034547c4fb7d9cbe3d03df74f6
Tyler Morton <tbmorton@ucsc.edu>
2025-04-12 08:34:54 -0700

    Fixed merge; rustdoc support

--- hhds/rust/hello-world/src/main.rs
+++ hhds/rust/hello-world/src/main.rs
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-fn main() {
-    println!("Hello, world!");
-=======
 use anyhow::Result;
 use serde_json::{json, Value};
 
@@ -9,5 +5,4 @@ fn main() -> Result<()> {
     println!("Hello, world!");
     let j: Value = json!("{}");
     Ok(())
->>>>>>> 49c0365bc7a75feb3f017d1296600bed77394d36
 }
