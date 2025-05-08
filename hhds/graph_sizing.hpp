db54aeb37d1a39c306ad33947dbe306b7b67b92f
Jose Renau <renau@ucsc.edu>
2025-05-08 14:25:55 -0700

    clang-format pass

--- hhds/graph_sizing.hpp
+++ hhds/graph_sizing.hpp
@@ -3,15 +3,15 @@
 
 namespace hhds {
 
-static constexpr int Nid_bits = 42;
+static constexpr int Nid_bits  = 42;
 static constexpr int Port_bits = 22;
 
-using Nid = uint64_t;
-using Pid = uint64_t;
-using Type = uint16_t;
+using Nid     = uint64_t;
+using Pid     = uint64_t;
+using Type    = uint16_t;
 using Port_id = uint32_t;
 
-static constexpr Nid Nid_invalid = (Nid(1) << Nid_bits) - 1;
+static constexpr Nid     Nid_invalid  = (Nid(1) << Nid_bits) - 1;
 static constexpr Port_id Port_invalid = (Port_id(1) << Port_bits) - 1;
 
-} // namespace hhds
+}  // namespace hhds
